9050a8c827ad62ce8a721d6351b428848817f89a
darty <darty@videogamesrock.com>
2020-02-06 13:40:28 -0500

    Changes to the benstring

--- setupShipBuilder.js
+++ setupShipBuilder.js
@@ -394,6 +394,8 @@ function generateBenString(){
 	if(loadout.AUGMENTATION.length){
 		benString = benString.substring(0,benString.length-1)
 	}
+	benString = benString.replace("null,","");
+	benString = benString.replace("null","");
 	window.alert(benString);
 }
 
